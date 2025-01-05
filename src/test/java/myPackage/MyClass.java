623f0cb7479fc1c7f87d087b3a22d6cdba819ec4
shrirang.ladda <shrirang.ladda@AWAMRCCHNLT0032.AIRWORKS.IN>
2025-01-05 11:16:41 +0530

    My commit on 05-01-2024

---
+++ src/test/java/myPackage/MyClass.java
@@ -0,0 +1,16 @@
+package myPackage;
+
+public class MyClass {
+
+	public void TestMethod() {
+		
+		System.out.println("My Test");
+	}
+	
+	public static void main(String[] args) {
+      MyClass TM = new MyClass();
+      TM.TestMethod();
+
+	}
+
+}
